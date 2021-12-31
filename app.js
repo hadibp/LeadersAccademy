570776df642f0d03e009afa321f0fc0003933733
hadi <abdulhadibp@gmail.com>
2021-12-31 18:45:05 +0530

    nav

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const app=express()
+var path = require('path');
 
+
+app.set('views',path.join(__dirname, 'views'))
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:false}))
+app.use('/public',express.static('public'));
 
 
 app.get('/',(req,res)=>{
     res.render('home');
 })
+app.get('/nav',(req,res)=>{
+    res.render('partials/logindashbord');
+})
 
-app.listen(3000,()=>{
+app.listen(4000,()=>{
     console.log("website is running");
 })
