6476191821cf4649b9f3785268dcb6007587d71a
hadi <abdulhadibp@gmail.com>
2022-02-13 19:11:22 +0530

    fixed nav issue

--- app.js
+++ app.js
@@ -3,10 +3,29 @@ const app=express()
 var path = require('path');
 
 
+// database connection
+var db=require('./config/connection')
+
+
+
+// routes
+
+var adminRouter = require('./routes/admin');
+var studentRouter = require('./routes/student');
+var publicRouter = require('./routes/public');
+
+
+
 app.set('views',path.join(__dirname, 'views'))
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:false}))
 app.use('/public',express.static('public'));
+
+// connection establish
+// db.connect((err)=>{
+//     if(err)console.log("connection error"+err);
+//     else console.log("Database connected to mongo");
+//   });
 
 
 app.get('/',(req,res)=>{
